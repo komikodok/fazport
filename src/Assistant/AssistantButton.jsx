bc2645bf981f8005654a6ea7521c02e2c20c95eb
komikodok <komikodok666@gmail.com>
2025-03-02 22:03:46 +0700

    minor update

--- src/Assistant/AssistantButton.jsx
+++ src/Assistant/AssistantButton.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import API_BASE_URL from "../config/apiConfig";
 
-const AssistantButton = ({openAssistant, setOpenAssistant}) => {
+const AssistantButton = ({handleClick, openAssistant, setOpenAssistant}) => {
     const [mouseOver, setMouseOver] = useState(false);
 
     return (
         <button className={`fixed cursor-pointer w-14 h-14 bottom-0 ring-0 md:left-10 rounded-full m-10 ${openAssistant ? "translate-y-30" : "translate-y-0"} transform transition-transform ease-out duration-400 md:duration-200 overflow-hidden`}>
             <img 
                 src={`${mouseOver ? "image1.png" : "image0.png"}`}
-                onClick={() => setOpenAssistant(prev => !prev)}
+                onClick={handleClick}
                 onMouseOver={() => setMouseOver(true)} 
                 onMouseLeave={() => setMouseOver(false)} 
             />
