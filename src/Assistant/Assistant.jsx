66dc8d07d8e89d724512de17f5d9089d3f930077
komikodok <komikodok666@gmail.com>
2025-03-11 20:44:51 +0700

    test

--- src/Assistant/Assistant.jsx
+++ src/Assistant/Assistant.jsx
@@ -4,6 +4,8 @@ import AssistantButton from "./AssistantButton";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log(`API URL(from assistant ): ${API_BASE_URL}`)
+console.log(`URL(from assistant ): ${BASE_URL}`)
 
 const Assistant = () => {
     const [openAssistant, setOpenAssistant] = useState(false);
