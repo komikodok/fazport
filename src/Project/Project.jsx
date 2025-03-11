07f5c97ae09012553576dcde7edbfc06eaf26e60
komikodok <komikodok666@gmail.com>
2025-03-11 21:30:18 +0700

    test

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import ProjectDetail from "./ProjectDetail";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log(`URL form Project: ${API_BASE_URL}`)
 
 const Project = () => {
     const [openProjectDetail, setOpenProjectDetail] = useState(false);
